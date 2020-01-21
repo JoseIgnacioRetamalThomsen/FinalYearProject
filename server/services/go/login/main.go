b8d2f4ce1eac77b04bc12d856008f048fb23ab0f
joseretamal <joseretamal@outlook.ie>
2020-01-21 12:21:52 +0000

    auth service to password service proto

--- server/services/go/login/main.go
+++ server/services/go/login/main.go
@@ -9,6 +9,9 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
+
+	//"time"
 
 	pb "github.com/joseignacioretamalthomsen/wcity"
 	"google.golang.org/grpc"
@@ -16,7 +19,13 @@ import (
 
 const (
 	port = ":50051"
+
 )
+
+//first addrs is the master
+var db_addrs = []string{"104.40.206.141:7777"}
+var ps_addrs = []string{"40.118.90.61:5701"}
+
 
 type server struct {
 	pb.UnimplementedUserAuthenticationServer
@@ -35,6 +44,9 @@ func (s *server) CreateUser(ctx context.Context, in *pb.UserRequest) (*pb.UserRe
 }
 
 func main() {
+
+	fmt.Print("helloworld")
+	fmt.Print(hash("helloworld"))
 	fmt.Print("hello")
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
@@ -45,4 +57,29 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+
+func hash(pass string) ([]byte,[]byte){
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(ps_addrs[0], grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewPasswordServiceClient(conn)
+
+	// Contact the server and print out its response.
+
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Hash(ctx, &pb.HashRequest{Password:pass})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+
+	return r.GetHashedPassword(), r.GetSalt()
 }
